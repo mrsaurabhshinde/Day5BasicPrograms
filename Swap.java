0d5296fe74060aca3778a87530d2a946a760f7cc
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-05 15:00:08 +0530

    Swaping two numbers

---
+++ Swap.java
@@ -0,0 +1,14 @@
+package com.bl.basicprogram;
+
+public class Swap
+{
+    public static void main(String[] args) {
+        int a=4;
+        int b=5;
+        System.out.println("Befor swaping : "+a +" " +b);
+        int swap1=b;
+        int swap2=a;
+        System.out.println("after swapping : "+swap1 +" " +swap2);
+
+    }
+}
